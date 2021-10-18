03433db9fcf37947ba8fecbbcc721aa071d0364b
Elsayed4 <39974553+Elsayed4@users.noreply.github.com>
2021-10-18 15:30:42 +0200

    third

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import './App.css';
 import React,{useState} from 'react';
 import Counter from './Counter.jsx';
 const App = () => {
-  const [count,setcount] = useState(0);
+  const [count,setcount] = useState(1);
+  const [fun,setfun] = useState('');
+  const [data,setdata] = useState(true);
 
   function decrement(){
     setcount(count - 1) 
   }
-  
   function increment(){
     setcount(count + 1) 
   }
   
-const [color,setColor] = useState('white');
+const [color,setColor] = useState('red');
 
   return (
     <div className="App">
-          <Counter count={count} 
-          increment={increment}
+          <Counter
+           count={count} 
            decrement={decrement}
+           increment={increment}
            color={color}
+           fun={fun}
+           data={data}
            ></Counter>
+           
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,16 @@
 import './App.css';
-import React,{useState} from 'react';
+import Countnumber from './Countnumber';
+
 const Counter = (props) => {
 
   return (
     <section className="Counter">
-        <button onClick = {props.decrement}> - </button>
-          <span class={props.color}>{props.count}</span>
-        <button onClick = {props.increment}> + </button>
+    <Countnumber action={props.increment} data={props.data}></Countnumber>
+    
+        <span className={ props.count >= 0 ? 'white':"red"}>{props.count}</span>
+        
+        <Countnumber action={props.decrement} data={!props.data}></Countnumber>
+        
         <p className="txtStatus">{props.count >= 0 ? 'positive Number' : 'Negative Number'}</p> 
     </section>
   );
